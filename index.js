c3ce48c3826cb532ddf8fc5df0f74718d3d06227
taniashahida-dev <taniashahida993@gmail.com>
2026-05-22 13:35:25 +0600

    Make api for delete cars

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { createRemoteJWKSet, jwtVerify } = require('jose-cjs')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 const uri = process.env.MONGODB_URI;
 
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //  const JWKS = createRemoteJWKSet(
 //       new URL('http://localhost:3000/api/auth/jwks')
 //     )
 // console.log(JWKS)
 
-app.get('/',(req,res)=>{
-    res.send('server runinggg')
-})
+app.get("/", (req, res) => {
+  res.send("server runinggg");
+});
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // const loggin =async(req,res,next)=>{
@@ -42,7 +41,6 @@ const client = new MongoClient(uri, {
 //     )
 //     const { payload } = await jwtVerify(token, JWKS, )
 
-
 //  req.user = payload
 // //  console.log( req.user)
 //     // return payload
@@ -52,151 +50,136 @@ const client = new MongoClient(uri, {
 //     res.status(401).json({message: 'Unautharized'})
 //   }
 
-
 //  console.log(token)
-    // console.log(authoraize)
-    // console.log(req)
+// console.log(authoraize)
+// console.log(req)
 
 // }
 
-const run =async()=>{
-try {
-    await client.connect()
-  
-    const db = client.db('drive-nest')
-    const carsCullection = db.collection('cars')
-    const bookingCullection = db.collection('booking')
-    const userCarCullection = db.collection('user-car')
+const run = async () => {
+  try {
+    await client.connect();
 
-
+    const db = client.db("drive-nest");
+    const carsCullection = db.collection("cars");
+    const bookingCullection = db.collection("booking");
+    const userCarCullection = db.collection("user-car");
 
     // get operation
-app.get('/car',async(req,res)=>{
+    app.get("/car", async (req, res) => {
+      const defaultCars = await carsCullection.find().toArray();
 
-// const {search}= req.query
-let cursor
+      const userCars = await userCarCullection.find().toArray();
 
-// if(search){
-//     cursor =coursCullection.find({title: { $eq: search}})
-// }else{
-cursor = carsCullection.find()
-// }
+      const allCars = [...defaultCars, ...userCars];
 
+      res.send(allCars);
+    });
 
-const result =await cursor.toArray()
-// console.log(result)
-res.send(result)
+    app.get("/available-cars", async (req, res) => {
+      const cursor = carsCullection.find().limit(8);
+      const result = await cursor.toArray();
+      // console.log(result)
+      res.send(result);
+    });
+
+    // get Operation by
+    app.get("/explore/:id", async (req, res) => {
+
+  const id = req.params.id
+
+  const query = {
+    _id: new ObjectId(id),
+  }
+
+  // first search in main cars
+  let result = await carsCullection.findOne(query)
+
+  // if not found search in user cars
+  if (!result) {
+    result = await userCarCullection.findOne(query)
+  }
+
+  res.send(result)
 })
 
-
-app.get('/available-cars',async(req,res)=>{
-const cursor = carsCullection.find().limit(4)
-const result =await cursor.toArray()
-// console.log(result)
-res.send(result)
-})
-
-
- 
-// get Operation by 
-app.get('/explore/:id' ,async(req,res)=>{
-
-   console.log(req?.user)
-    const id = req.params.id
-    const query= {
-        _id: new ObjectId(id)
-    }
-    const result = await carsCullection.findOne(query)
-    res.send(result)
-})
-
-
-
-// Post operation
-// app.post('/users' ,async(req,res)=>{
-//     const newUser= req.body
-//    console.log('new user is come',newUser)
-//     const result = await userCullection.insertOne(newUser)
-//     res.send(result)
-// })
-
-
-
-
-
-
-
-
-// car bookings 
-
-app.post('/bookings' , async (req,res)=>{
-    const bookingData = req.body
-     const result = await bookingCullection.insertOne(bookingData)
-     console.log(result)
-    res.send(result)
-})
-app.get('/bookings' , async (req,res)=>{
    
-     const result = await bookingCullection.find().toArray()
-     console.log(result)
-    res.send(result)
-})
+    // car bookings
 
-//user added cars api 
-app.post('/user-cars' , async (req,res)=>{
-    const carData = req.body
-     const result = await userCarCullection.insertOne(carData)
-     console.log(result)
-    res.send(result)
-})
-app.get('/user-cars' , async (req,res)=>{
-   
-     const result = await userCarCullection.find().toArray()
-     console.log(result)
-    res.send(result)
-})
+    app.post("/bookings", async (req, res) => {
+      const bookingData = req.body;
+      const result = await bookingCullection.insertOne(bookingData);
 
+      res.send(result);
+    });
+    app.get("/bookings/:email", async (req, res) => {
+      const { email } = req.params;
 
-// patch operation
-app.patch('/user-cars/:id' ,async(req,res)=>{
-    const id= req.params.id
-   console.log('new car is come',id)
-const filter={
-  _id: new ObjectId(id)
-}
-const newCarData = req.body
-    const updateDocument ={
-         $set:  newCarData
-   
-    }
-    console.log("After update",newUser)
-    const result = await userCarCullection.updateOne(filter,updateDocument)
-    res.send(result)
-})
+      const result = await bookingCullection
+        .find({
+          userEmail: email,
+        })
+        .toArray();
 
+      res.send(result);
+    });
 
-// Delete operation
-app.delete('/user-cars/:id' ,async(req,res)=>{
-    const id = req.params.id
-    const query= {
-        _id: new ObjectId(id)
-    }
-    const result = await userCarCullection.deleteOne(query)
-    res.send(result)
-})
+    //user added cars api
+    app.post("/user-cars", async (req, res) => {
+      const carData = req.body;
+      const result = await userCarCullection.insertOne(carData);
+      console.log(result);
+      res.send(result);
+    });
+    app.get("/user-cars/:email", async (req, res) => {
+      const { email } = req.params;
+      const result = await userCarCullection
+        .find({
+          userEmail: email,
+        })
+        .toArray();
+      console.log(result);
+      res.send(result);
+    });
 
+    // patch operation
+    app.patch("/user-cars/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("new car is come", id);
+      const filter = {
+        _id: new ObjectId(id),
+      };
+      const newCarData = req.body;
+      const updateDocument = {
+        $set: newCarData,
+      };
+      // console.log("After update", newCarData);
+      const result = await userCarCullection.updateOne(filter, updateDocument);
+      res.send(result);
+    });
 
+    // Delete operation
+    app.delete("/user-cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await userCarCullection.deleteOne(query);
+      res.send(result);
+    });
 
+    
 
-
-    await client.db('admin').command({ping:1})
-     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-}finally {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
     // await client.close()
-}
-}
-run().catch(console.dir)
+  }
+};
+run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log(`Simple crud server is rinnung from  on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Simple crud server is rinnung from  on port ${port}`);
+});
